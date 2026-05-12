0d900e7ee0acae3a78674564c3aa7eea1d9e78de
Eric Pekmah <pekstaar@gmail.com>
2026-05-12 09:49:25 +0300

    Restore Android SMS native module

---
+++ android/app/src/main/java/com/hauzpay/monisens/sms/MonisensSmsPackage.kt
@@ -0,0 +1,18 @@
+package com.hauzpay.monisens.sms
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class MonisensSmsPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(MonisensSmsModule(reactContext))
+  }
+
+  override fun createViewManagers(
+    reactContext: ReactApplicationContext,
+  ): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
